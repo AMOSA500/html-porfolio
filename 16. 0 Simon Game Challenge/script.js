1385804124b982384fb4f88370f14dd56ff53455
UncleNAF <66817828+AMOSA500@users.noreply.github.com>
2024-07-09 12:47:12 +0100

    Checked user answers and created a highest score when the game ends

--- 16. 0 Simon Game Challenge/script.js
+++ 16. 0 Simon Game Challenge/script.js
@@ -1,27 +1,82 @@
-$(document).ready(function(){
-    var gamePattern = [];
-    var btnColor = ["red", "blue", "green", "yellow"];
-    var randNum = Math.floor(Math.random() * 4); // 
-    function nextSequence(i){
-        if (i == undefined) {
-            i = randNum;
-        }
-        var randSelectedColor = btnColor[i];
-        $("#"+randSelectedColor).fadeOut(100).fadeIn(100);
-        playSound(randSelectedColor);
+var gamePattern = [];
+var userClickedPattern = [];
+var displayText = "Simon Game Level "
+var btnColor = ["red", "blue", "green", "yellow"];
+var started = false;
+var counter = 0;
+$(document).keydown(function(e){
+    if (started == false){
+        $("#level-title").text(displayText+counter);
+        started = true;
     }
-
-    function playSound(color){
-        var audio = new Audio("sounds/"+color+".mp3");
-        audio.play();
-    }
-
-    $(".box").click(function(){
-        var userChosenColor = this.id;
-        gamePattern.push(userChosenColor);
-        var colorIndex = btnColor.indexOf(userChosenColor);
-        nextSequence(colorIndex);
-    })
-    nextSequence();
 });
+
+function nextSequence(i){
+    var randNum = Math.floor(Math.random() * 4); // 
+    var randSelectedColor = btnColor[randNum];
+    gamePattern.push(randSelectedColor);
+    $("#"+randSelectedColor).fadeOut(100).fadeIn(100);
+    if(started == true){
+        $("#level-title").text(displayText+(++counter));
+    }
+
+    playSound(randSelectedColor);
+}
+
+function playSound(color){
+    var audio = new Audio("sounds/"+color+".mp3");
+    //audio.play();
+}
+
+$(".box").click(function(){
+    if (started == true){
+        var userChosenColor = this.id;
+        userClickedPattern.push(userChosenColor);
+        playSound(userChosenColor);
+
+        // animatePress
+        animatePress(userChosenColor);
+        console.log(userClickedPattern);
+        console.log(gamePattern);
+
+        // Check answer
+        if (userClickedPattern.length == gamePattern.length){
+            checkAnswer();
+        }
+    }else{
+        alert("Press any key to start the game");
+    }
+});
+
+function checkAnswer(){
+    if(userClickedPattern.toString() == gamePattern.toString()){
+        
+       // Call next color
+        setTimeout(function(){
+            nextSequence();
+            userClickedPattern = [];
+        },1000);
+    }else{
+        $("#score").text(counter);
+        var failedSound = new Audio("sounds/wrong.mp3");
+        failedSound.play();
+        $("body").addClass("game-over");
+        $("#level-title").text("Game Over, Restart in 2 seconds").css("color","black");
+        $(".score").css("color","black");
+        setTimeout(function(){ 
+            // Reset the game
+            location.reload();
+
+        }, 2000);
+        
+    }
+}
+function animatePress(currentColor){
+    $("#"+currentColor).addClass("pressed");
+    setTimeout(function(){
+        $("#"+currentColor).removeClass("pressed");
+    },100);
+}
+nextSequence();
+
 
